9aba11447781077a36b3501ce8e35dcc079a3d6b
inakilafuente <inaki.lafuente@circulargo.com>
2023-10-01 12:15:06 +0200

    Extra_02

---
+++ Caso_practico_01/extra_2.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Vamos a realizar una pequeña vista consumiendo una API externa
+ * https://633dc0417e19b17829155626.mockapi.io/api/employees
+ *
+ * La URL anterior nos devuelve una lista de empleados
+ * 1. Consulta la URL para obtener el json
+ * 2. Transforma el contenido recibido en "algo" manejable por PHP
+ * 3. Pinta cada uno de los empleados por pantalla
+ *      [ID, imagen, nombre, ciudad, empresa, fecha de creación en formato Español]
+ * --> Opción fácil, pintar por consola. Las imágenes no se verán, así que filtra la url y muestra sólo el nombre "1128.jpg"
+ * --> Opción algo menos fácil, pintar en un HTML simple accediendo desde el navegador. Sin complicaciones
+ *
+ * EXTRA:
+ * La API admite varios parámetros de filtrado: https://github.com/mockapi-io/docs/wiki/Code-examples#filtering
+ * Ajusta tu script para admitir parámetros opcionales y ejecutarlo de este modo:
+ *      php consume_api.php [pagina] [limite] [busqueda]
+ *      Ejemplo: "php consume_api.php 2 5 ar"
+ *      Ejecutar la línea anterior debería llamar a la API incluyendo lo siguiente:
+ *      https://633dc0417e19b17829155626.mockapi.io/api/employees?page=2&limit=5&search=ar
+ *
+ * Investiga cómo leer los parámetros recibidos por consola o navegador ;)
+ * Navegador:
+ *  a) http://localhost:8000/01.%20Sintaxis%20PHP/04_caso_practico/extra2.php/2/5/ar
+ *  b) http://localhost:8000/01.%20Sintaxis%20PHP/04_caso_practico/extra2.php?page=2&limit=5&search=ar
+ *
+ * NOTA: No olvides sanear los parámetros recibidos ;)
+ */
+
+$url='https://633dc0417e19b17829155626.mockapi.io/api/employees';
+function obtener_datos(string $url,$pagina=false,$limite=false,$busqueda=false){
+    if($pagina||$limite||$busqueda){
+        $url.="?";
+    }
+    if($pagina){
+        $url=$url."&page=".$pagina;
+    }
+    if($limite){
+        $url=$url."&limit=".$limite;
+    }
+    if($busqueda){
+        $url=$url."&search=".$busqueda;
+    }
+    echo("Estas consultando esta URL: ".$url."\n");
+    $ch=curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    $respuesta=curl_exec($ch);
+    if(curl_errno($ch)){
+        $error_msg=curl_errno(($ch));
+        echo("Error al conectarse a la API");
+    }else{
+        curl_close($ch);
+
+        $data=json_decode($respuesta,true);
+        foreach ($data as $empleado){
+            echo("::::::::::::::::::::::::::::::::::::\n");
+            echo("Empleado: ". $empleado['id'] ."\n");
+            echo("Imagen: ". $empleado['avatar'] ."\n");
+            echo("Nombre: ". $empleado['name'] ."\n");
+            echo("Ciudad: ". $empleado['city'] ."\n");
+            echo("Empresa: ". $empleado['company'] ."\n");
+            $timestamp=$empleado['createdAt'];
+            $dateTime = date('Y-m-d H:i:s', $timestamp);
+            echo("Fecha Creacion: ". $dateTime ."\n");
+            echo("::::::::::::::::::::::::::::::::::::\n");
+        }
+    }
+}
+
+$pagina=0;
+$limite=0;
+$busqueda="";
+if(($argc)>1 && ($argc<4)){
+    echo("Parametros recibidos: \n");
+    for($i=0;$i<$argc;$i++){
+        if($i==0){
+            $pagina=(int) $argc[$i];
+        }
+        if($i==1){
+            $limite=(int) $argc[$i];
+        }
+        if($i==2){
+            $busqueda=(string) $argc[$i];
+        }
+    }
+}else{
+    echo("Ningun parametro introducido \n");
+}
+obtener_datos($url,$pagina,$limite,$busqueda);
